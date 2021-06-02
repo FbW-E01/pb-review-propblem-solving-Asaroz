08e742a9279d41c1056261e3b8cfa0358b9faab0
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-06-02 10:33:43 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,3 @@
-
 // "Client order"
 
 // Your client SpaceY is creating a new AI system that tries to detect patterns in data using their new algorithm.
@@ -17,30 +16,40 @@
 // - endDate:string - the last date when your data should be created (to 23:59:59)
 // Also, add a comment where you report how long does it take to generate 10 million lines of data.
 
-function create_data(idLength,startDate,endDate){
-    let number=""
-    for(i=0;i<idLength;i++){
-        number+= Math.floor(Math.random()*10)
-    }
-    
+function create_data(idLength, startDate, endDate) {
     let start = new Date(startDate)
     let end = new Date(endDate)
-    let randomDate= new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
-    const date_obj = {day:"2-digit",month:"2-digit",year:"numeric",hour:"2-digit",minute:"2-digit",second:"2-digit" }
-         const data={
-            id:number,
-            time: randomDate.toLocaleString('DE', date_obj)+" UTC"
+    const array = []
+    let number = ""
+    const date_obj = {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    }
+    const datetimeformat = Intl.DateTimeFormat('DE', date_obj)
+    for (y = 0; y < 10000000; y++) {
+        let randomDate = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()))
+        for (i = 0; i < idLength; i++) {
+            number += Math.floor(Math.random() * 10)
         }
-        return data
+        array.push({
+            id:number,
+            time: datetimeformat.format(randomDate)+" UTC"
+        })
+        number = ""
+    }
+    return array
 }
 
 console.time("test");
-const array=[]
-for(y=0;y<10000000;y++){
-    array.push(create_data(8,"December 17, 1995 03:24:00","June 28, 2017 07:55:00"))
-}
+let fin_array = create_data(8, "December 17, 1995 03:24:00", "June 28, 2017 07:55:00")
 console.timeEnd("test")
-
+console.log(fin_array)
 //test: 16:49.101 (m:ss.mmm)
 // test two without console.log 
 //test: 31.211s
+
+//test three is 32.43 with correct format
